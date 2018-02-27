29b717fb78254b32aabe2648e8745648e54a96b3
Antoine Chabert <chabert.antoine@gmail.com>
2018-02-27 20:15:13 +0100

    WIP - Add mailer service

---
+++ src/AppBundle/Service/MailerService.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Participation;
+use Doctrine\ORM\EntityManager;
+use FOS\UserBundle\Mailer\MailerInterface;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Class MailerService
+ * @package AppBundle\Service
+ */
+class MailerService implements MailerInterface
+{
+    /** @var \Swift_Mailer */
+    protected $mailer;
+    /** @var  EntityManager */
+    protected $em;
+    /** @var  \Twig_Environment */
+    protected $template;
+    /** @var array */
+    protected $parameters;
+    /** @var UrlGeneratorInterface */
+    protected $router;
+
+    /**
+     * MailerService constructor.
+     *
+     * @param \Swift_Mailer         $mailer
+     * @param EntityManager         $em
+     * @param \Twig_Environment     $template
+     * @param UrlGeneratorInterface $router
+     * @param array                 $parameters
+     */
+    public function __construct(\Swift_Mailer $mailer, EntityManager $em, \Twig_Environment $template, UrlGeneratorInterface $router, $parameters)
+    {
+        $this->mailer = $mailer;
+        $this->em = $em;
+        $this->template = $template;
+        $this->router = $router;
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Send an email to the participant to get his approval
+     *
+     * @param Participation $participation
+     * @return bool true if succeed
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function sendParticipantApproval(Participation $participation)
+    {
+        $template = $this->template->render('email/participant_approval.html.twig', [  // TODO create the template
+            'participation' => $participation,
+        ]);
+
+        return $this->sendMessage(
+            $template,
+            '[Croissants Bringer] - Demande de participation pour le ' . $participation->getDate()->format('d-m-Y'),
+            $this->parameters['email']['noreply'], // TODO put the parameter
+            $participation->getUser()->getEmail()
+        );
+    }
+
+    /**
+     * Send an email to a user to confirm the account creation
+     *
+     * @param UserInterface $user
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function sendConfirmationEmailMessage(UserInterface $user)
+    {
+        $url = $this->router->generate('fos_user_registration_confirm', ['token' => $user->getConfirmationToken()], UrlGeneratorInterface::ABSOLUTE_URL);
+        $template = $this->template->render('email/reset_password.html.twig', // TODO create the template
+            [
+                'confirmationUrl' => $url,
+                'user' => $user,
+            ]);
+        $this->sendMessage(
+            $template,
+            '[Croissants Bringer] - Confirmation', // TODO translate
+            $this->parameters['email']['noreply'],
+            $user->getEmail()
+        );
+    }
+    /**
+     * Send an email to a user to confirm the password reset
+     *
+     * @param UserInterface $user
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function sendResettingEmailMessage(UserInterface $user)
+    {
+        $url = $this->router->generate('fos_user_resetting_reset', ['token' => $user->getConfirmationToken()],
+            UrlGeneratorInterface::ABSOLUTE_URL);
+        $template = $this->template->render('email/reset_password.html.twig', // TODO create the template
+            [
+                'confirmationUrl' => $url,
+                'user' => $user,
+            ]);
+        $this->sendMessage(
+            $template,
+            '[Croissants Bringer] - Demande de changement de mot de passe', // TODO translate
+            $this->parameters['email']['noreply'],
+            $user->getEmail()
+        );
+    }
+
+    /**
+     * Send mail
+     *
+     * @param string $body
+     * @param string $subject
+     * @param string|array $fromEmail
+     * @param string|array $to
+     * @param array $cc
+     * @param array $bcc
+     *
+     * @return bool true if sent, false if failed
+     */
+    private function sendMessage($body, $subject, $fromEmail, $to, $cc = [], $bcc = [])
+    {
+        $message = \Swift_Message::newInstance()
+            ->setSubject($subject)
+            ->setFrom($fromEmail);
+        if (is_array($to)) {
+            foreach ($to as $email) {
+                $message->setTo($email);
+            }
+        } else {
+            $message->setTo($to);
+        }
+        foreach ($cc as $email) {
+            $message->setCC($email);
+        }
+        foreach ($bcc as $email) {
+            $message->setBcc($email);
+        }
+        $message->setBody($body,'text/html');
+
+        return 0 !== $this->mailer->send($message);
+    }
+}
